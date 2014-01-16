760d132ee1eac2f0819d5b0f147153869a2eee2f
John Perl <johnrperl@gmail.com>
2014-01-16 14:13:44 -0700

    Adding my johnperl-code.js

---
+++ johnperl-code.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: JRPerl
+ * Date: 1/16/14
+ * Time: 2:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//This is in my johnperl-code.js for the develop branch
