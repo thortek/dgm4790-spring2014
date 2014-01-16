a080356e3c46b23f62bbf60ded805b3622caed85
Tyler Maynard <tymayn@gmail.com>
2014-01-16 14:11:21 -0700

    Thor's Hammer

--- thors-code.js
+++ thors-code.js
@@ -1,3 +1,21 @@
 //This is where I would put my javascript code
 
 // This is a comment that belongs only in my develop branch
+
+/*
+
+
+ThorTek's Mighty Coding Hammer
+
+      (/T\)  _____       
+      (-,-) ()Thor) .  + 
+      \(o)/   -|3 .     .
+     /='"'===//||   .    
+,OOO//|   |    "" +   .  
+O:O:O LLLLL + .  .   .   
+\OOO/ || ||     .  .   .
+     C_) (_D              
+
+     -Tyler Maynard  
+
+     /*
