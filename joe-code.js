1706b60607b9719d411861c0b7d8762b09b4f844
William Stanton <gomi28@hotmail.com>
2014-01-17 05:05:52 -0700

    Who wrote Hey Joe?

--- joe-code.js
+++ joe-code.js
@@ -3,3 +3,5 @@
 //I have hi-jacked your file.
 
 //Hey joe, where you goin' with that gun in your hand
+
+//Claimed by the late Tim Rose to be a traditional song, or often erroneously attributed to the pen of American musician Dino Valenti (who also went by the names Chester or Chet Powers, and Jesse Farrow)
