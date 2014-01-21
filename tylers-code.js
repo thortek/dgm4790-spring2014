02b4dc688177f99fec3ff12a68c80917925a9f78
Tyler Maynard <tymayn@gmail.com>
2014-01-21 13:37:04 -0700

    Added Tylers Object

---
+++ tylers-code.js
@@ -0,0 +1,13 @@
+//create Tyler object
+var tmObj = {}
+
+//give my object some properties
+tmObj.info {
+    fname: "tyler",
+    lname: "maynard",
+    address: "338 E Shamrock",
+    school: "Utah Valley University"
+}
+
+// test the object
+console.table(tmObj.info);
