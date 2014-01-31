08f7bac144a23510ee73b3a3673894ea9b733139
james <toney04@gmail.com>
2014-01-30 22:45:15 -0700

    "added properties to Tyler's object"

--- tylers-code.js
+++ tylers-code.js
@@ -1,13 +1,16 @@
 //create Tyler object
-var tmObj = {}
+var tmObj = {};
 
 //give my object some properties
-tmObj.info {
+tmObj.info = {
     fname: "tyler",
     lname: "maynard",
     address: "338 E Shamrock",
-    school: "Utah Valley University"
-}
+    school: "Utah Valley University",
+    hair:"brown",
+    job:"Zion's Bank",
+    hobbies:['Not video games', "coding", "Banking"]
+};
 
 // test the object
 console.table(tmObj.info);
