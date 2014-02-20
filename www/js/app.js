9952c16b74a475e2063fbed927a267f7c0167046
TylersDesk <tymayn@gmail.com>
2014-02-20 00:09:51 -0700

    Task 2.7 - Building authentication services and dean/anon state

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ngCookies', 'starter.services', 'starter.controllers'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -14,114 +14,95 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
+
+  //Define access from routingConfig.js
+  var access = routingConfig.accessLevels;
+
   $stateProvider
     // setup a login state
-    .state('tab', {
-      url: "/tab",
+    .state('anon', {
       abstract: true,
-      templateUrl: "templates/tabs.html",
+      template: "<nav-view/>",
+      data : {
+        access: access.anon
+      }
+    })
+
+    .state('anon.login', {
+      url: "/login/",
+      templateUrl: 'templates/login.html',
       controller: 'loginCtrl'
     })
 
-    .state('tab.login', {
-      url: "/login",
-      views: {
-        'login-tab': {
-          templateUrl: 'templates/login.html',
-          controller: 'loginCtrl'
-        }
-      }
-    })
+    .state('anon.register', {
+      url: '/register/',
+      templateUrl: 'templates/register.html',
+      controller: 'registerCtrl'
+    });
 
-    .state('tab.register', {
-      url: '/register',
-      views: {
-        'register-tab': {
-          templateUrl: 'templates/register.html',
-          controller: 'registerCtrl'
-        }
-      }
-    })
-
-    .state('tab.register-details', {
-      url: '/register/1',
-      views: {
-        'register-tab': {
-          templateUrl: 'templates/register.1.html',
-          controller: 'registerCtrl'
-        }
-      }
-    })
-
-    .state('tab.register-confirm', {
-      url: '/register/2',
-      views: {
-        'register-tab': {
-          templateUrl: 'templates/register.2.html',
-          controller: 'registerCtrl'
-        }
-      }
-    })
-
-    // .state('register.1', {
-    //   url: "/1",
-    //   views: {
-    //     'register': {
-    //       templateUrl: '/templates/register.1.html',
-    //       controller: 'registerCtrl'
-    //     }
-    //   }
-    // })
-
-    // // setup an abstract state for the tabs directive
-    // .state('tab', {
-    //   url: "/tab",
-    //   abstract: true,
-    //   templateUrl: "templates/tabs.html"
-    // })
-
-    // // the pet tab has its own child nav-view and history
-    // .state('tab.pet-index', {
-    //   url: '/pets',
-    //   views: {
-    //     'pets-tab': {
-    //       templateUrl: 'templates/pet-index.html',
-    //       controller: 'PetIndexCtrl'
-    //     }
-    //   }
-    // })
-
-    // .state('tab.pet-detail', {
-    //   url: '/pet/:petId',
-    //   views: {
-    //     'pets-tab': {
-    //       templateUrl: 'templates/pet-detail.html',
-    //       controller: 'PetDetailCtrl'
-    //     }
-    //   }
-    // })
-
-    // .state('tab.adopt', {
-    //   url: '/adopt',
-    //   views: {
-    //     'adopt-tab': {
-    //       templateUrl: 'templates/adopt.html'
-    //     }
-    //   }
-    // })
-
-    // .state('tab.about', {
-    //   url: '/about',
-    //   views: {
-    //     'about-tab': {
-    //       templateUrl: 'templates/about.html'
-    //     }
-    //   }
-    // })
-  ;
+    $stateProvider
+      .state('user', {
+        abstract: true,
+        template: '<nav-view/>'
+      })
+      .state('user.home', {
+        url: '/',
+        templateUrl: '/templates/dean-home.html'
+      })
+    ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/login');
+  $urlRouterProvider.otherwise('/login/');
 
-});
+  // FIX for trailing slashes. Gracefully "borrowed" from https://github.com/angular-ui/ui-router/issues/50
+    $urlRouterProvider.rule(function($injector, $location) {
+        if($location.protocol() === 'file')
+            return;
+
+        var path = $location.path()
+        // Note: misnomer. This returns a query object, not a search string
+            , search = $location.search()
+            , params
+            ;
+
+        // check to see if the path already ends in '/'
+        if (path[path.length - 1] === '/') {
+            return;
+        }
+
+        // If there was no search string / query params, return with a `/`
+        if (Object.keys(search).length === 0) {
+            return path + '/';
+        }
+
+        // Otherwise build the search string and return a `/?` prefix
+        params = [];
+        angular.forEach(search, function(v, k){
+            params.push(k + '=' + v);
+        });
+        return path + '/?' + params.join('&');
+    });
+
+})
+
+//Run this off the bat to determine state and act accordingly
+.run(['$rootScope', '$state', 'Auth', function ($rootScope, $state, Auth) {
+
+    $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+        if (!Auth.authorize(toState.data.access)) {
+            $rootScope.error = "Seems like you tried accessing a route you don't have access to...";
+            event.preventDefault();
+            
+            if(fromState.url === '^') {
+                if(Auth.isLoggedIn())
+                    $state.go('user.home');
+                else {
+                    $rootScope.error = null;
+                    $state.go('anon.login');
+                }
+            }
+        }
+    });
+
+}]);
 
