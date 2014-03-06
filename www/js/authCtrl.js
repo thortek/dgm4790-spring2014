ac6a41684fe62951fe92abf5a1c783dbbf588511
TylersDesk <tymayn@gmail.com>
2014-03-06 14:42:05 -0700

    AuthCtrol added

---
+++ www/js/authCtrl.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers', [])
+
+// Auth Ctrl
+.controller('authCtrl', function($scope, authService) {
+  // "Login" is a service returning mock data (services.js)
+  console.log('Auth fired');
+});
