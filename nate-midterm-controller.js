aafabfc5b7d07e2a020b34784e8e3ed036e516cc
thedarkknate <jediknate@gmail.com>
2014-03-05 20:17:25 -0700

    I finished my stuff. Love, Nate.

---
+++ nate-midterm-controller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var carApp = angular.module('carApp', []);
+
+carApp.controller('carController',
+	function carController($scope){
+
+		$scope.event = {
+     cars: [{
+     manufacturer: 'Porsche',
+     model: '911',
+     price: 135000,
+     wiki: 'http://en.wikipedia.org/wiki/Porsche_997'
+ },{
+     manufacturer: 'Nissan',
+     model: 'GT-R',
+     price: 80000,
+     wiki:'http://en.wikipedia.org/wiki/Nissan_Gt-r'
+ },{
+     manufacturer: 'BMW',
+     model: 'M3',
+     price: 60500,
+     wiki:'http://en.wikipedia.org/wiki/Bmw_m3'
+ },{
+     manufacturer: 'Audi',
+     model: 'S5',
+     price: 53000,
+     wiki:'http://en.wikipedia.org/wiki/Audi_S5#Audi_S5'
+ }]
+}
+
+
+	}
+);//end of app controller
