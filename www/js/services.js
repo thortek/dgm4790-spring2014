bd1b0191fc95c28aec4cb91d8fe2cfddb5eb9e9d
Chad <chadwtkns@gmail.com>
2014-03-27 13:38:51 -0600

    Cleaned up a bit of code

--- www/js/services.js
+++ www/js/services.js
@@ -84,7 +84,7 @@ angular.module('starter.services', ['ngResource'])
             }
         }
     })
-    .factory('eventStartDateService', function($http,$q,eventService){
+    .factory('eventStartDateService', function(eventService){
         return{
             startDateEvent:'value.startDate'
         };
